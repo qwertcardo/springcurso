9a25037e19184090708252e45b67456b2140c051
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-07-08 10:27:02 -0300

    Criado Domain/Service/Repository Categoria

---
+++ src/main/java/com/qwertcardo/springcurso/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.qwertcardo.springcurso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qwertcardo.springcurso.domain.Categoria;
+import com.qwertcardo.springcurso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
