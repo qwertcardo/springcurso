399c5ebc04b9e2ab807646ad00a9c70d4cf3d9cf
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-07-09 00:04:54 -0300

    implements CommandLineRunner para teste

--- src/main/java/com/qwertcardo/springcurso/SpringcursoApplication.java
+++ src/main/java/com/qwertcardo/springcurso/SpringcursoApplication.java
@@ -1,13 +1,33 @@
 package com.qwertcardo.springcurso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringcursoApplication {
+import com.qwertcardo.springcurso.domain.Categoria;
+import com.qwertcardo.springcurso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringcursoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
