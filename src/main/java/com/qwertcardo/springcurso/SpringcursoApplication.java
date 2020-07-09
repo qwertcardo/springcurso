b8133a01b04313715ad12d8c715f440ab37a278c
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-07-09 09:25:06 -0300

    Alterado strategy de todas as entidades, criado novos Domain

--- src/main/java/com/qwertcardo/springcurso/SpringcursoApplication.java
+++ src/main/java/com/qwertcardo/springcurso/SpringcursoApplication.java
@@ -8,44 +8,63 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.qwertcardo.springcurso.domain.Categoria;
+import com.qwertcardo.springcurso.domain.Cidade;
+import com.qwertcardo.springcurso.domain.Estado;
 import com.qwertcardo.springcurso.domain.Produto;
 import com.qwertcardo.springcurso.repositories.CategoriaRepository;
+import com.qwertcardo.springcurso.repositories.CidadeRepository;
+import com.qwertcardo.springcurso.repositories.EstadoRepository;
 import com.qwertcardo.springcurso.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class SpringcursoApplication implements CommandLineRunner{
+public class SpringcursoApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcursoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
 		Produto p3 = new Produto(null, "Mouse", 80.0);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
-		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
 	}
 
 }
---
+++ src/main/java/com/qwertcardo/springcurso/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.qwertcardo.springcurso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qwertcardo.springcurso.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
