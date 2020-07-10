0da15a023ab4598436cab96fad2dc16333b30093
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-07-10 09:11:07 -0300

    ItemPedido/ItemPedidoPK

--- src/main/java/com/qwertcardo/springcurso/SpringcursoApplication.java
+++ src/main/java/com/qwertcardo/springcurso/SpringcursoApplication.java
@@ -13,6 +13,7 @@ import com.qwertcardo.springcurso.domain.Cidade;
 import com.qwertcardo.springcurso.domain.Cliente;
 import com.qwertcardo.springcurso.domain.Endereco;
 import com.qwertcardo.springcurso.domain.Estado;
+import com.qwertcardo.springcurso.domain.ItemPedido;
 import com.qwertcardo.springcurso.domain.Pagamento;
 import com.qwertcardo.springcurso.domain.PagamentoComBoleto;
 import com.qwertcardo.springcurso.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.qwertcardo.springcurso.repositories.CidadeRepository;
 import com.qwertcardo.springcurso.repositories.ClienteRepository;
 import com.qwertcardo.springcurso.repositories.EnderecoRepository;
 import com.qwertcardo.springcurso.repositories.EstadoRepository;
+import com.qwertcardo.springcurso.repositories.ItemPedidoRepository;
 import com.qwertcardo.springcurso.repositories.PagamentoRepository;
 import com.qwertcardo.springcurso.repositories.PedidoRepository;
 import com.qwertcardo.springcurso.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class SpringcursoApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcursoApplication.class, args);
@@ -112,6 +116,19 @@ public class SpringcursoApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
--- src/main/java/com/qwertcardo/springcurso/domain/Produto.java
+++ src/main/java/com/qwertcardo/springcurso/domain/Produto.java
@@ -2,7 +2,10 @@ package com.qwertcardo.springcurso.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,13 +14,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-public class Produto implements Serializable{
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -26,11 +30,11 @@ public class Produto implements Serializable{
 
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name = "produto_categoria", 
-		joinColumns = @JoinColumn(name = "produto_id"), 
-		inverseJoinColumns = @JoinColumn(name = "categoria_id")
-	)
+	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 
@@ -41,6 +45,12 @@ public class Produto implements Serializable{
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+
+	public List<Pedido> getPedidos() {
+		List<Pedido> list = itens.stream().map(
+				x -> x.getPedido()).collect(Collectors.toList());
+		return list;
 	}
 
 	public Integer getId() {
@@ -73,6 +83,14 @@ public class Produto implements Serializable{
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
